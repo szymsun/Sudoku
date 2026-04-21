fb2c2994d638bcaeefe5f5f7c343b23f0d9eec5e
xd <szy.gladysz@gmail.com>
2026-04-21 13:15:27 +0200

    Dodaj pliki projektów.

---
+++ SudokuBackend/SudokuBackend.cpp
@@ -0,0 +1,42 @@
+﻿#include <iostream>
+#include <vector>
+
+
+struct CellGroup {
+private:
+	int cellData[3][3];
+
+public:
+	int GetData(const int& X, const int& Y) {
+		return cellData[Y][X];
+	}
+
+	void SetData(const int& X,const int& Y, int data) {
+		cellData[Y][X] = data;
+	}
+};
+
+CellGroup GenerateCellGroup() {
+	CellGroup group;
+
+	for (size_t Y = 0; Y < 3; Y++)
+	{
+		for (size_t X = 0; X < 3; X++)
+		{
+			group.SetData(X,Y,-1);
+		}
+	}
+
+	return group;
+}
+
+int main()
+{
+	std::vector<std::vector<CellGroup>> sheet = {
+												{GenerateCellGroup()},
+												{GenerateCellGroup()},
+												{GenerateCellGroup()},
+	};
+
+}
+
