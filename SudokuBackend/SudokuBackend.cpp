1a803ad574f5dcac93aacdf72fb9ac572edf1a03
xd <szy.gladysz@gmail.com>
2026-04-21 14:35:35 +0200

    WIP 1 restructured many stuff
     - simplified structure of puzzle
    - removed some redurant stuff (there is still more left)
    
    thinking bout switching to cmake

--- SudokuBackend/SudokuBackend.cpp
+++ SudokuBackend/SudokuBackend.cpp
@@ -1,10 +1,14 @@
 ﻿#include <iostream>
 #include <vector>
+#include <cstdlib>	
+#include <ctime>   
+#include "String"
 
+#define GAME_DIFF 6
 
 struct CellGroup {
 private:
-	int cellData[3][3];
+	int cellData[9][9];
 
 public:
 	int GetData(const int& X, const int& Y) {
@@ -14,29 +18,53 @@ public:
 	void SetData(const int& X,const int& Y, int data) {
 		cellData[Y][X] = data;
 	}
+
+	std::string ToString() {
+		std::string out; 
+
+		for (size_t Y = 0; Y < 9; Y++)
+		{
+			for (size_t X = 0; X < 9; X++)
+			{
+				out += std::to_string(cellData[Y][X]);
+			}
+
+			out += "\n";
+		}
+
+		return out;
+	}
 };
 
 CellGroup GenerateCellGroup() {
 	CellGroup group;
 
-	for (size_t Y = 0; Y < 3; Y++)
+	for (size_t Y = 0; Y < 9; Y++)
 	{
-		for (size_t X = 0; X < 3; X++)
+		for (size_t X = 0; X < 9; X++)
 		{
-			group.SetData(X,Y,-1);
+			if (std::rand() % GAME_DIFF <= GAME_DIFF - 2) {
+				
+			}
 		}
 	}
 
 	return group;
 }
 
+
+
 int main()
 {
-	std::vector<std::vector<CellGroup>> sheet = {
-												{GenerateCellGroup()},
-												{GenerateCellGroup()},
-												{GenerateCellGroup()},
-	};
+	std::srand(std::time(0));
+
+	std::cout << std::rand() % 9 << "\n";
+
+	CellGroup board = GenerateCellGroup();
+
+	
+
+	std::cout << board.ToString();
 
 }
 
